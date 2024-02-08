8b5924b64314e9f96fb1fcd1b9591f02bdb045a6
Juan Rosas <rosas.juan@live.com>
2024-02-08 16:15:29 -0300

    done but checking

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 require('dotenv').config();
 const express = require('express');
-const app = express();
 const cors = require('cors');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const app = express();
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -22,20 +20,16 @@ app.get('/api/hello', function(req, res) {
 });
 
 // URL Shortener Microservice
-app.use(bodyParser.urlencoded({ extended: false }));  // Allows to read the body of the Post request
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }); //Connects to Mongoose db
-
-// Create a new db schema or db structure
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));  
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 const urlSchema = new mongoose.Schema({
   original_url: { type: String, required: true },
   short_url: Number
 });
-
-// Create the db model
 const Url = mongoose.model('Url', urlSchema);
-
 let urlCounter = 0;
-// Endpoint post request to extract the url from the body of the message
 app.post('/api/shorturl', async (req, res) => {
   const originalUrl = req.body.url;
   const urlRegex = /^(http|https):\/\/[^ "]+$/;
@@ -60,7 +54,6 @@ app.post('/api/shorturl', async (req, res) => {
     res.status(500).json('Server Error');
   }
 });
-
 app.get('/api/shorturl/:short_url', async (req, res) => {
   const shortUrl = req.params.short_url;
   try {
@@ -75,6 +68,8 @@ app.get('/api/shorturl/:short_url', async (req, res) => {
   }
 });
 
+
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
+
